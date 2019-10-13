20f862ef5b2706c39c01e45e316d865524f9beb1
ArturPravdivtsev <33483323+ArturPravdivtsev@users.noreply.github.com>
2019-10-13 13:11:55 +0300

    Add files via upload

---
+++ src/Car.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>Car</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam quo eius labore. Doloremque quae facere incidunt consequuntur est, ipsam natus possimus aperiam, aspernatur id dolores sequi iure dolor, quaerat quos.</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/Counter.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>Counter: {{ counter }}</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur et voluptatum quisquam optio harum, quibusdam expedita recusandae quos ad ipsum. Illum inventore numquam corporis iste corrupti. Aut, tempora quod. Odit.</p>
+    </div>
+</template>
+
+<script>
+export default {
+   
+}
+</script>
